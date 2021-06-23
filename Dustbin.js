f84f9cec1021cfe1f3fbb60d1ded24343e3780f2
Barenya <73180779+Barenya2007@users.noreply.github.com>
2021-06-23 21:35:54 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -14,7 +14,7 @@ class Dustbin{
         push()
         translate(this.body.position.x,this.body.position.y);
         imageMode(CENTER);
-        image(this.image,0,0,this.width,this.height)
+        image(this.image,0,-this.height/2,this.width,this.height)
         pop()
        
     }
